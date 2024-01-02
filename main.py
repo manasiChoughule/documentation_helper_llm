20ccb6a5810aad2d9cbf4a198094c367d8be3e1d
manasiChoughule <mchoughule96@gmail.com>
2024-01-02 00:31:51 -0500

    Implemented interface

---
+++ main.py
@@ -0,0 +1,48 @@
+from backend.core import run_llm
+import streamlit as st
+from typing import Set
+from streamlit_chat import message
+
+st.header("Documentation Helper Bot")
+
+prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
+
+if "user_prompt_history" not in st.session_state:
+    st.session_state["user_prompt_history"] = []
+
+if "chat_answers_history" not in st.session_state:
+    st.session_state["chat_answers_history"] = []
+
+
+def create_source_string(source_urls: Set[str]) -> str:
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
+
+
+if prompt:
+    with st.spinner("Generating Response..."):
+        generated_response = run_llm(query=prompt)
+        sources = set(
+            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+        )
+
+        formatted_response = (
+            f"{generated_response['result']} \n\n {create_source_string(sources)}"
+        )
+        # store the prompt and answer history
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+
+if st.session_state["chat_answers_history"]:
+    for user_query, generated_response, in zip(
+        st.session_state["user_prompt_history"],
+        st.session_state["chat_answers_history"],
+    ):
+        message(user_query, is_user=True)
+        message(generated_response)
