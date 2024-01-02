1c1919ed3918546fc0a78067063d4127af924f11
manasiChoughule <mchoughule96@gmail.com>
2024-01-02 02:16:55 -0500

    Added memory

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answers_history" not in st.session_state:
     st.session_state["chat_answers_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
 
 def create_source_string(source_urls: Set[str]) -> str:
     if not source_urls:
@@ -27,17 +30,20 @@ def create_source_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating Response..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_source_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_source_string(sources)}"
         )
         # store the prompt and answer history
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
     for (
